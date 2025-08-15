9a39bee3b99b83ba4dd388871c914d7b8e130b3a
IsaacGridGains Dev <isaac_akinladejo@hotmail.com>
2025-08-15 11:40:09 +0100

    Playing around with python

---
+++ MySQLServer.py
@@ -0,0 +1,20 @@
+import mysql.connector
+from mysql.connector import Error
+
+#Creating the Database
+try:
+	connection = mysql.connector.connect(
+		host = "localhost",
+		user = "root",
+		password = "pass_word"
+	)
+	mycursor = connection.cursor()
+	mycursor.execute("CREATE DATABASE alx_book_store")
+except Error as e:
+	print("Error Creating Database", e)
+	
+else:
+	print("Database Created successfully")
+finally:
+	mycursor.close()
+	connection.close()
